d98578f2e7bb5483c55414f1da6a6b768879237b
TurnerHelical <hunterde91@gmail.com>
2025-03-14 13:22:19 -0400

    created function to push bookObj to library array

--- scripts.js
+++ scripts.js
@@ -9,12 +9,16 @@ function Book(title, author, pages, read) {
     // create a function that generates a unique ID for each book and adds it to the book object on creation, can be created with crypto.randomUUID()
     this.id = crypto.randomUUID();
 }
-    
-// take the created book objects and push it into a Library array
 
-// make a function that display the books on the page, with cards
+
+let test = new Book('test','test',24,'yes');
+
+// take the created book objects and push them into a Library array
+function addBookToLibrary(bookObj) {
+    library.push(bookObj);
+}
+// make a function that displays the books on the page, with cards
 // have a button that will bring up a form and allow a user to add a new book to the library array
 // have another button on the book cards that removes the book from the library array
 // and one more button to change the read property on the book
 
-let test = new Book('test','test',24,'yes');
