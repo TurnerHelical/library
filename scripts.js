b3795e54384f6573c25f83504f8620fc27241296
Hunter <hunterde91@gmail.com>
2025-03-27 18:14:42 -0400

    working on adding book cards with javascript

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 // initialize the library array
 let library = [];
+
+const bookContainer = document.querySelector('.bookContainer')
 // create a constructor function to add new books, include title, author, # of pages, and if you've read it, and unique id
 function Book(title, author, pages, read, image) {
     this.title = title;
@@ -12,13 +14,22 @@ function Book(title, author, pages, read, image) {
 }
 
 
-let test = new Book('test','test',24,'yes');
+let test = new Book('test','test',23,'yes','https://gratisography.com/wp-content/uploads/2025/03/gratisography-cruising-cat-800x525.jpg');
+let test1 = new Book('test','test',24,'yes',);
 
 // take the created book objects and push them into a Library array
 function addBookToLibrary(bookObj) {
     library.push(bookObj);
 }
 // make a function that displays the books on the page, with cards
+function addBookCardToPage(book) {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('card');
+    const bookImage = document.createElement('img');
+    bookImage.src = ((book.image === undefined) ? './images/libraryImage.jpg' : book.image );
+    bookCard.appendChild(bookImage);
+    bookContainer.appendChild(bookCard);
+}
 // have a button that will bring up a form and allow a user to add a new book to the library array
 // have another button on the book cards that removes the book from the library array
 // and one more button to change the read property on the book
