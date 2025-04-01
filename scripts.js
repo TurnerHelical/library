8b649bf1489ddf75d2ade5808705c1e2fed5332a
TurnerHelical <hunterde91@gmail.com>
2025-04-01 10:34:59 -0400

    functionality works as intended, just need to add validation and check for dupes

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@
 const container = document.querySelector('#container');
 const bookContainer = document.querySelector('.bookContainer');
 const addBook = document.querySelector('#addBook');
-
-addBook.addEventListener('click', function(e) {
+let finished = '';
+addBook.addEventListener('click', function (e) {
     addBookForm();
 });
 // create a constructor function to add new books, include title, author, # of pages, and if you've read it, and unique id
@@ -20,12 +20,12 @@ function Book(title, author, pages, read, image) {
 
 
 
-let test = new Book('test1','test',23,'yes','https://gratisography.com/wp-content/uploads/2025/03/gratisography-cruising-cat-800x525.jpg');
-let test1 = new Book('test2','test',24,'no',);
-let test2 = new Book('test3','test',24,'no',);
-let test3 = new Book('test4','test',24,'no',);
-let test4 = new Book('test5','test',24,'no',);
-let test5 = new Book('test6','test',24,'no',);
+let test = new Book('test1', 'test', 23, 'yes', 'https://gratisography.com/wp-content/uploads/2025/03/gratisography-cruising-cat-800x525.jpg');
+let test1 = new Book('test2', 'test', 24, 'no',);
+let test2 = new Book('test3', 'test', 24, 'no',);
+let test3 = new Book('test4', 'test', 24, 'no',);
+let test4 = new Book('test5', 'test', 24, 'no',);
+let test5 = new Book('test6', 'test', 24, 'no',);
 
 let library = [];
 
@@ -38,7 +38,7 @@ function addBookCardToPage(book) {
     const bookCard = document.createElement('div');
     bookCard.classList.add('card');
     const bookImage = document.createElement('img');
-    bookImage.src = ((book.image === undefined) ? './images/libraryImage.jpg' : book.image );
+    bookImage.src = ((book.image === undefined) ? './images/libraryImage.jpg' : book.image);
     bookCard.appendChild(bookImage);
     const bookTitle = document.createElement('h3');
     bookTitle.textContent = book.title;
@@ -47,7 +47,7 @@ function addBookCardToPage(book) {
     const bookPages = document.createElement('p');
     bookPages.textContent = 'Pages: ' + book.pages;
     const bookRead = document.createElement('p');
-    bookRead.textContent = 'Finished Reading: '+ book.read;
+    bookRead.textContent = 'Finished Reading: ' + book.read;
     const buttonContainer = document.createElement('div');
     buttonContainer.id = 'cardButtonContainer';
     const readButton = document.createElement('button');
@@ -58,11 +58,11 @@ function addBookCardToPage(book) {
     deleteButton.textContent = 'Delete';
     if (book.read === 'no')
         buttonContainer.appendChild(readButton);
-        readButton.addEventListener('click', function (e) {
-            book.read = 'yes';
-            bookRead.textContent = 'Finished Reading: '+ book.read;
-            readButton.remove(this);
-        })
+    readButton.addEventListener('click', function (e) {
+        book.read = 'yes';
+        bookRead.textContent = 'Finished Reading: ' + book.read;
+        readButton.remove(this);
+    })
     buttonContainer.appendChild(deleteButton);
     bookCard.appendChild(bookTitle);
     bookCard.appendChild(bookAuthor);
@@ -77,18 +77,29 @@ function addBookCardToPage(book) {
 }
 // have a button that will bring up a form and allow a user to add a new book to the library array
 function addBookForm() {
-   const modal = document.querySelector('#formContainer');
-   const content = document.querySelector('#container');
-   modal.classList.remove('modalClose');
-   content.classList.add('modalOpen');
-   const submitButton = document.querySelector('#formSubmit');
-   submitButton.addEventListener('click', function (e) {
-        theOne();
-   })
-   const cancelButton = document.querySelector('#formCancel');
-   cancelButton.addEventListener('click', function(e) {
+    const modal = document.querySelector('#formContainer');
+    const content = document.querySelector('#container');
+    modal.classList.remove('modalClose');
+    content.classList.add('modalOpen');
+    const haveRead = document.querySelector('#read');
+    const notRead = document.querySelector('#notRead');
+    haveRead.addEventListener('change', function (e) {
+        return finished = 'yes';
+    })
+    notRead.addEventListener('change', function (e) {
+        return finished = 'no';
+    })
+    const submitButton = document.querySelector('#formSubmit');
+    submitButton.addEventListener('click', function (e) {
+        theOne(finished);
+        
+    })
+    const cancelButton = document.querySelector('#formCancel');
+    cancelButton.addEventListener('click', function (e) {
         closeBookForm();
-        });
+    });
+
+
 }
 
 function closeBookForm() {
@@ -98,3 +109,20 @@ function closeBookForm() {
     content.classList.remove('modalOpen');
 }
 // have another button on the book cards that removes the book from the library array
+// Pass the data from the form submit to the object constructor and use that information to add cards to the page
+function theOne(finished) {
+    const titleField = document.querySelector('#title');
+    const authorField = document.querySelector('#author');
+    const pagesField = document.querySelector('#pages');
+    const imageField = document.querySelector('#image');
+    let bookTitle = titleField.value;
+    let authorName = authorField.value;
+    let pagesNumber = pagesField.value;
+    let imageURL = imageField.value; 
+
+    let newBook = new Book(bookTitle, authorName, pagesNumber,finished, imageURL);
+    closeBookForm();
+    addBookToLibrary(newBook);
+    addBookCardToPage(newBook);
+    
+}
